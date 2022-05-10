10979b08640634ff0adf978e428197407863f473
RudeZwloki7 <rudezwloki7@gmail.com>
2022-05-10 21:04:39 +0300

    Color buttons...

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,22 +1,26 @@
 <script>
     import { Button, Navbar, NavbarBrand } from "sveltestrap";
+    import "./Header.css";
 
     let is_authorized = false;
+    let color = "#ff3e00";
 
-    function handleAuth(){
-        is_authorized = !is_authorized
+    function handleAuth() {
+        is_authorized = !is_authorized;
     }
 </script>
 
 <Navbar>
-    <NavbarBrand  style="color: #ff3e00; text-decoration: none;">Nota Bene</NavbarBrand>
+    <NavbarBrand style="color: {color}; text-decoration: none;"
+        >Nota Bene</NavbarBrand
+    >
     <div>
         {#if !is_authorized}
-            <Button on:click={handleAuth}>Login</Button>
-            <Button>Register</Button>
-        {:else}
-            <h4 class='d-inline p-2'>Hello, user</h4>
-            <Button on:click={handleAuth}>Logout</Button>
+            <Button style="background-color: {color};" on:click={handleAuth}>Login</Button>
+            <Button style="background-color: {color};">Register</Button>
+        {:else} 
+            <h4 class="d-inline p-2">Hello, user</h4>
+            <Button style="background-color: {color};" on:click={handleAuth}>Logout</Button>
         {/if}
     </div>
 </Navbar>
